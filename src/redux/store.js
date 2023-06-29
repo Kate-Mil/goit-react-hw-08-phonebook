98075f4702cc2d5af2a4af1b5ad03a952718e444
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-06-29 12:26:17 +0300

    add logic of redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice, contactsSlice, filterSlice } from './index';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistedAuthSlice = persistReducer(
+  { key: 'auth', storage, whiteList: ['token'] },
+  authSlice.reducer
+);
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer,
+    auth: persistedAuthSlice,
     contacts: contactsSlice.reducer,
     filter: filterSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
